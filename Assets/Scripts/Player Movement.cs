8b56f831558fedbbafc63167dcf7debda6afbee2
ArianaNChaves <navi.ariana99@gmail.com>
2024-08-30 10:33:14 -0300

    feat: restrict players movement

--- Assets/Scripts/Player Movement.cs
+++ Assets/Scripts/Player Movement.cs
@@ -14,6 +14,9 @@ public class Movement : MonoBehaviour
     private bool _canMove;
     private KeyCode _keyUp;
     private KeyCode _keyDown;
+
+    private bool _canMoveUp;
+    private bool _canMoveDown;
     
     private enum Player
     {
@@ -29,8 +32,8 @@ public class Movement : MonoBehaviour
                 _keyDown = KeyCode.S;
                 break;
             case Player.Player2: //todo Como haria esto correctamente? Porque el sprite quiero que mire <- pero eso cambia si esta rotado
-                _keyUp = KeyCode.DownArrow;
-                _keyDown = KeyCode.UpArrow;
+                _keyUp = KeyCode.UpArrow;
+                _keyDown = KeyCode.DownArrow;
                 break;
             default:
                 Debug.LogError("Player Movement - Awake - Player no asignado");
@@ -44,22 +47,44 @@ public class Movement : MonoBehaviour
     void Update()
     {
         Move();
+        RestrictMovement();
     }
     private void Move()
     {
         if (!_canMove) return;
         _distance = Vector2.zero;
-        if (Input.GetKey(_keyUp))
+        if (Input.GetKey(_keyUp) && _canMoveUp)
         {
             _distance += Vector2.left; //todo Ver si esto esta bien asi o como rotar el sprite
         }
-        if (Input.GetKey(_keyDown))
+        if (Input.GetKey(_keyDown) && _canMoveDown)
         {
             _distance += Vector2.right; //todo lo mismo se aplica
         }
 
         Vector2 move = _distance.normalized;
         transform.Translate(move * (speed * Time.deltaTime));
+    }
+
+    private void RestrictMovement()
+    {
+        if (transform.position.y <= 4.3f)
+        {
+            _canMoveUp = true;
+        }
+        else
+        {
+            _canMoveUp = false;
+        }
+
+        if (transform.position.y >= -4.3f)
+        {
+            _canMoveDown = true;
+        }
+        else
+        {
+            _canMoveDown = false;
+        }
     }
     public void SetPlayerSpeed(float newSpeed)
     {
